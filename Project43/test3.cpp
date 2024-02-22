5ed6c0513aa7a128120aa5029d5e9126ecd0ee06
EvgenDev09 <eugene.lippa@gmail.com>
2024-02-22 17:16:03 +0300

    task03

---
+++ Project43/test3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void change(int& number) {
+	number = number * 2;
+
+	//a = a * 2;
+	//a *= 2
+}
+
+int main() {
+	int num = 100;
+
+	cout << "Before num = " << num << endl;
+
+	change(num);
+
+	cout << "After num = " << num << endl;
+
+
+	return 0;
+}
