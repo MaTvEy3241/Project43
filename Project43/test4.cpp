94c40ef7d4506dd6472a00e34ff3b426d3a7c0bd
EvgenDev09 <eugene.lippa@gmail.com>
2024-02-22 17:25:57 +0300

    task05

--- Project43/test4.cpp
+++ Project43/test4.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-
-using namespace std;
-void swap(int a, int b) {
-	int t = a;
-	a = b;
-	b = t;
-}
-
-int main() {
-	int x = 24;
-	int y = 34;
-
-	cout << "Before x = " << x << "y = " << y << endl;
-
-	swap(x,y);
-
-	cout << "Before x = " << x << "y = " << y << endl;
-
-
-
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//void swap(int a, int b) {
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//int main() {
+//	int x = 24;
+//	int y = 34;
+//
+//	cout << "Before x = " << x << "y = " << y << endl;
+//
+//	swap(x,y);
+//
+//	cout << "Before x = " << x << "y = " << y << endl;
+//
+//
+//
+//	return 0;
+//}
